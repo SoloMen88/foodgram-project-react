1a9ad18939658a910de524282e98b46a3dc23933
Стас Кучеренко <solomen88@ya.ru>
2022-10-03 19:50:20 +0300

    7

--- backend/foodgram_api/settings.py
+++ backend/foodgram_api/settings.py
@@ -62,8 +62,6 @@ WSGI_APPLICATION = 'foodgram_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
